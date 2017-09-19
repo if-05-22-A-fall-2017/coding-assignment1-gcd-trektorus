2e4e6cd4129f7f889f848f0df192586a80b9d595
Thomas Baminger <thms.baminger@gmail.com>
2017-09-19 12:04:12 +0200

    Finished the program

--- GCD/src/gcd/GCD.java
+++ GCD/src/gcd/GCD.java
@@ -5,6 +5,9 @@
  */
 package gcd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Thomas
@@ -15,7 +18,49 @@ public class GCD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(gcdPrimeFactors(30, 45));
+        System.out.println(gcdEucledian(30, 45));
     }
     
+    public static int gcdEucledian(int a, int b) { return b == 0 ? a : gcdEucledian(b, a % b); }
+    
+    public static int gcdPrimeFactors(int a, int b) {
+        int gcd = 1;
+        List<Integer> aFactors = getPrimeFactors(a);
+        List<Integer> bFactors = getPrimeFactors(b); 
+        List<Integer> biggerList;
+        List<Integer> smallerList;
+        if (aFactors.size() > bFactors.size()) {
+            biggerList = aFactors;
+            smallerList = bFactors;
+        } else {
+            biggerList = bFactors;
+            smallerList = aFactors;
+        }
+        for (int i = 0; i < smallerList.size(); i++) {
+            for (int j = 0; j < biggerList.size(); j++) {
+                if (smallerList.get(i).compareTo(biggerList.get(j)) == 0) {
+                    gcd *= smallerList.get(i);
+                    biggerList.remove(j);
+                    break;
+                }
+            }
+        }
+        return gcd;
+    }
+    
+    private static List<Integer> getPrimeFactors(int n) {
+        int number = n;
+        List<Integer> factors = new ArrayList<>();
+        for (int i = 2; i <= number / i; i++) {
+            while (number % i == 0) {
+                factors.add(i);
+                number /= i;
+            }
+        }
+        if (number > 1) {
+            factors.add(number);
+        }
+        return factors;
+    }
 }
